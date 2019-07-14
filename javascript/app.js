ad325d84cdc999f207f70d542a16e616b2e1d239
Ricardo Perdomo <hndgd1980@gmail.com>
2019-07-14 18:05:22 -0500

    added links to firebase and moments.js

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBiEpvBpunUVTCaXZn0M45oyG82QGvBMWA",
+    authDomain: "test-afb4b.firebaseapp.com",
+    databaseURL: "https://test-afb4b.firebaseio.com",
+    projectId: "test-afb4b",
+    storageBucket: "",
+    messagingSenderId: "798898889652",
+    appId: "1:798898889652:web:9b6e79821799b5af"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
